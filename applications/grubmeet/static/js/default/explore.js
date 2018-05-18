1400529c041839f082a24cf76bc1fba83483e55c
Unknown <dtang10@ucsc.edu>
2018-05-17 18:23:50 -0700

    Update .gitignore

---
+++ applications/grubmeet/static/js/default/explore.js
@@ -0,0 +1,12 @@
+$('#explore_tab').addClass("active");
+$('.special.cards .image').dimmer({
+    on: 'hover'
+});
+
+function more_detail_modal() {
+    $('#popup')
+        .modal('show');
+    $('.slider').glide();
+
+}
+
