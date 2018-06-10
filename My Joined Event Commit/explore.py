aace9f03f3c2b51c9858f2598948030045695376
Arndtman <38261540+Arndtman@users.noreply.github.com>
2018-06-10 13:06:12 -0700

    Joined Events Commit
    Joined Events Commit 
    Since I had to pack it, I included the files I modified directly

---
+++ My Joined Event Commit/explore.py
@@ -0,0 +1,56 @@
+import random
+import time
+
+
+# Mocks implementation.
+
+def get_user_name_from_email(email):
+    """Returns a string corresponding to the user first and last names,
+    given the user email."""
+    u = db(db.auth_user.email == email).select().first()
+    if u is None:
+        return 'None'
+    else:
+        return ' '.join([u.first_name, u.last_name])
+
+@auth.requires_signature(hash_vars=False)
+def get_memos():
+    start_idx = int(request.vars.start_idx) if request.vars.start_idx is not None else 0
+    end_idx = int(request.vars.end_idx) if request.vars.end_idx is not None else 0
+    # We just generate a lot of of data.
+    memolist = []
+    has_more = False
+
+    # end_idx+1 is a trick to see if there's more items
+    rows = db().select(db.checklist.ALL, limitby=(start_idx, end_idx + 1))
+
+    # enumerate automatically assigns an index i to item r
+    for i, r in enumerate(rows):
+        # check if there is 1 more item
+        name = get_user_name_from_email(r.user_email)
+        if i < end_idx - start_idx:
+            t = dict(
+                id=r.id,
+                user_email=r.user_email,
+                title=r.title,
+                memo=r.memo,
+                datetime=r.datetime,
+                area=r.area,
+                time_of_event=r.time_of_event,
+                allergens=r.allergens,
+                time=r.updated_on,
+                is_public=r.is_public,
+                is_being_edited=False,
+                first_name=name,
+            )
+            memolist.append(t)
+        else:
+            has_more = True
+    logged_in = auth.user is not None
+
+    # below are data paramters when getJSON is called
+    return response.json(dict(
+        memolist=memolist,
+        logged_in=logged_in,
+        has_more=has_more,
+    ))
